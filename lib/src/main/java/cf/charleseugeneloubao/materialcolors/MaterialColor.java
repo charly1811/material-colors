9933233816d9c2c475ea00bfb3fef907a8204284
Charles-Eugene Loubao <charlesloubao95@gmail.com>
2015-10-31 01:11:11 -0400

    Initial commit

---
+++ lib/src/main/java/cf/charleseugeneloubao/materialcolors/MaterialColor.java
@@ -0,0 +1,16 @@
+package cf.charleseugeneloubao.materialcolors;
+
+/**
+ * Copyright 2015 Charles-Eugene Loubao
+ */
+public class MaterialColor {
+
+    public final int normal;
+    public final int dark;
+
+    public MaterialColor(int normal, int dark) {
+        this.normal = normal;
+        this.dark = dark;
+    }
+
+}
---
+++ lib/src/main/java/cf/charleseugeneloubao/materialcolors/MaterialColorRandom.java
@@ -0,0 +1,32 @@
+package cf.charleseugeneloubao.materialcolors;
+
+import android.content.Context;
+
+import java.util.Random;
+
+import cf.charleseugeneloubao.materialcolors.MaterialColor;
+import cf.charleseugeneloubao.materialcolors.R;
+
+/**
+ * Copyright 2015 Charles-Eugene Loubao
+ */
+public class MaterialColorRandom {
+
+    private Random random = new Random();
+
+    private int[] allColors;
+
+    private int[] primaryColors;
+    private int[] darkColors;
+
+    public MaterialColorRandom(Context context) {
+        primaryColors = context.getResources().getIntArray(R.array.primary_colors);
+        darkColors = context.getResources().getIntArray(R.array.dark_colors);
+    }
+
+
+    public MaterialColor nextMaterialColor() {
+        int index = random.nextInt(primaryColors.length);
+        return new MaterialColor(primaryColors[index],darkColors[index]);
+    }
+}
